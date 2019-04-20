b880f5da9dd963148c0c46e59f3ca7ef9534fb78
Sergey Mironenko <seregasmiron@mail.ru>
2019-04-20 16:56:20 +0300

    изучение работы с DataFrame

---
+++ testPandas/useDataFrame.py
@@ -0,0 +1,8 @@
+from pandas import DataFrame as df
+
+# создание фрейма
+frame1 = df({
+    'country': ['Kazakhstan', 'Russia', 'Belarus', 'Ukraine'],
+    'population': [17.04, 143.5, 9.5, 45.5],
+    'square': [2724902, 17125191, 207600, 603628]})
+print(frame1)
