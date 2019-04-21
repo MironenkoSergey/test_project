0171c39e18eb90d8a5952ce4fd15fa3e93b7a292
Sergey Mironenko <seregasmiron@mail.ru>
2019-04-21 16:30:50 +0300

    изучение операций DataFrame и сохранение его в .csv

---
+++ testPandas/Titanic.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+# считывание данных из csv
+titanik_df = pd.read_csv('titanic.csv')
+# подсчитаем выжевших пассажиров по полу и классу каюты
+print("Выжившие по полу")
+print(titanik_df.groupby(['Sex', 'Survived'])['PassengerID'].count(), '\n')
+print("Выжившие по классу каюты")
+print(titanik_df.groupby(['PClass', 'Survived'])['PassengerID'].count(), '\n')
+# сводные таблицы
+# подсчитать количество женщин в каютах каждого класса
+pvt = titanik_df.pivot_table(index='Sex', columns='PClass',
+                             values='Name', aggfunc='count')
+print("Женщин в каждом классе")
+print(pvt.loc['female', ['1st', '2nd', '3rd']], '\n')
+#
