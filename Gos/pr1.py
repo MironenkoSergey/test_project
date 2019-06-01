91a09a745527d019a211773a459ee0fe4b9054f3
Mironenko Sergey <49811310+MironenkoSergey@users.noreply.github.com>
2019-06-01 13:33:48 +0300

    pr1: fixed bugs

--- Gos/pr1.py
+++ Gos/pr1.py
@@ -3,7 +3,9 @@ def replace_min_max(path_file=None):
         print("\nВведите числа через ','\n")
         matrix = [list(input("Строка "+str(i)+":").split(',')) for i in range(10)]
     else:
-        matrix = [list(row.replace('\n', '').split(',')) for row in open(path_file, 'r').readlines()]
+        file = open(path_file, 'r')
+        matrix = [list(row.replace('\n', '').split(',')) for row in file.readlines()]
+        file.close()
     # all values to int
     matrix = list(map(lambda x: list(map(int, x)), matrix))
 
@@ -15,10 +17,12 @@ def replace_min_max(path_file=None):
             matrix[i] = list(map(lambda x: maximum if x == minimum else (minimum if x == maximum else x), matrix[i]))
 
     if path_file is None:
-        _ = [print(i) for i in matrix]
+        for i in matrix:
+            print(i)
     else:
         file = open(path_file, 'w')
-        _ = [file.write(str(i)[1:len(str(i))-1] + '\n') for i in matrix]
+        for i in matrix:
+            file.write(str(i)[1:len(str(i))-1] + '\n')
         file.close()
 
 
